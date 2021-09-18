1939fc55da26b1442e4112d7f580e521321bea17
Gabriel Luis Santos Freire <gabriellsf@gmail.com>
2021-09-18 12:44:15 -0300

    Saving blocked channels

--- AltCensored Channel Table/social_blade_scrapper_selenium.py
+++ AltCensored Channel Table/social_blade_scrapper_selenium.py
@@ -4,11 +4,12 @@ from selenium.webdriver.common.keys import Keys
 import os
 import time
 
-def write_csv(folder, file, data):
+def write_csv(folder, file, data, overwrite = True):
     dir = f"./data/{folder}"
     if not os.path.exists(dir):
         os.makedirs(dir)
-    with open(f'{dir}/{file}.csv', 'w', encoding="utf-8", newline='') as csv_file:
+    parameter = 'w' if overwrite else 'a'
+    with open(f'{dir}/{file}.csv', parameter, encoding="utf-8", newline='') as csv_file:
         writer = csv.writer(csv_file) 
         if data is not None:
             for d in data:
@@ -18,14 +19,21 @@ default_url_preffix = r'https://socialblade.com/'
 url_preffix = r'youtube/channel/'
 default_url_suffix = r'/monthly'
 
+blocked = []
+blocked_file = "./data/block/0.csv"
+if os.path.exists(blocked_file):
+    with open(blocked_file, 'r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            blocked.append(row[0])
 
 driver = webdriver.Firefox()
 
-skip = 433
+skip = 1
 stop_at = 100000
 at = -1
 with open('channels_altcensored.csv', 'r', encoding='utf-8') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in reader:
         
         at = at + 1
@@ -38,12 +46,20 @@ with open('channels_altcensored.csv', 'r', encoding='utf-8') as csvfile:
             print(f"Já coletado: {row[1]}" )
             continue
 
+        if row[1] in blocked:
+            print(f"Não coletado por bloqueio: {row[1]}" )
+            continue
+        
+
         url = default_url_preffix + url_preffix + row[1] + default_url_suffix
 
         try:
             driver.get(url)
         
             chart_quantity = driver.execute_script("return Highcharts.charts.length")
+            if chart_quantity == 0:
+                write_csv('block','0',[[row[1]]], overwrite=False)
+                continue
             chart_title_array = []
             for chart_idx in range(chart_quantity):
                 chart_title = driver.execute_script(f"return Highcharts.charts[{chart_idx}].title.textStr")
@@ -53,6 +69,8 @@ with open('channels_altcensored.csv', 'r', encoding='utf-8') as csvfile:
             write_csv(row[1],"dados_coletados",chart_title_array)
         except:
             print(row[1])
+            driver.close()
             time.sleep(10)
+            driver = webdriver.Firefox()
             continue
 driver.close()
